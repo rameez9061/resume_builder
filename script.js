ff997916c0d31c129716a34689038d01076c605d
Rameez Aziz <122540665+rameez9061@users.noreply.github.com>
2024-09-14 19:16:02 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -4,21 +4,21 @@ var toggleSkillsBtn = document.getElementById("toggle-skills-btn");
 var profilePicInput = document.getElementById('profile-pic');
 var profilePicPreview = document.querySelector('.profile-picture img');
 var skillsVisible = true;
-// Function to handle the image upload and display it in the preview
+
 profilePicInput.addEventListener('change', function () {
     var _a;
     var file = (_a = profilePicInput.files) === null || _a === void 0 ? void 0 : _a[0]; // Get the uploaded file
     if (file) {
-        var reader = new FileReader(); // Use FileReader to read the uploaded file
+        var reader = new FileReader(); 
         reader.onload = function (e) {
             var _a, _b;
             console.log("File read successfully:", (_a = e.target) === null || _a === void 0 ? void 0 : _a.result);
             profilePicPreview.src = (_b = e.target) === null || _b === void 0 ? void 0 : _b.result;
         };
-        reader.readAsDataURL(file); // Read the file as a Data URL (base64)
+        reader.readAsDataURL(file); 
     }
 });
-// Handle form submission to generate the resume preview
+
 form.addEventListener("submit", function (event) {
     event.preventDefault();
     console.log("Form submitted");
@@ -34,12 +34,12 @@ form.addEventListener("submit", function (event) {
     console.log("Name:", name);
     console.log("Email:", email);
     console.log("Profile Pic Source:", profilePicPreview.src);
-    // Generate the resume output HTML
+    
     var resumeHTML = "\n        <div class=\"profile-picture\">\n            <img src=\"".concat(profilePicPreview.src, "\" alt=\"Profile Picture\">\n        </div>\n        <h3>").concat(name, "</h3>\n        <p><strong>Email:</strong> ").concat(email, "</p>\n        <p><strong>Education:</strong></p>\n        <p>").concat(education, "</p>\n        <p id=\"skills-section\"><strong>Skills:</strong></p>\n        <ul id=\"skills-list\">").concat(skills.split(",").map(function (skill) { return "<li>".concat(skill.trim(), "</li>"); }).join(''), "</ul>\n        <p><strong>Experience:</strong></p>\n        <p>").concat(experience, "</p>\n    ");
-    // Display the resume output
+   
     resumeOutput.innerHTML = resumeHTML;
 });
-// Toggle the visibility of the Skills section
+
 toggleSkillsBtn.addEventListener("click", function () {
     var skillsSection = document.getElementById("skills-section");
     var skillsList = document.getElementById("skills-list");
